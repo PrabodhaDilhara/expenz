9b7ff383056e12672ca1e222907e9d7eb57ef5d2
PrabodhaDilhara <prabodhadilhara17@gmail.com>
2024-07-14 01:36:02 +0530

    Add custom fontd

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,25 @@
-import 'package:expenz/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: "Expenz",
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        fontFamily: "Inter",
       ),
-      home: const HomePage(),
+      home: Scaffold(
+        body: Center(
+          child: Text("Hellow flutter"),
+        ),
+      ),
     );
   }
 }
